d8248c8172e8cbc7c272e479bc6f990272c09d7b
miyagaw61 <miyagaw61@gmail.com>
2018-05-01 22:30:35 +0900

    Add start/stop subcommand

--- src/main.rs
+++ src/main.rs
@@ -82,83 +82,65 @@ fn process(command: &str) -> std::process::ExitStatus {
     child.wait().unwrap()
 }
 
-fn mining(_sdone: chan::Sender<()>, json_data: serde_json::Value) {
+fn mining(json_data: serde_json::Value) {
     let mut addresses_counter = 0;
     loop {
         {
             let mut address = ADDRESS.write().unwrap();
             *address = json_data["addresses"][addresses_counter].to_string();
+            if (address.len() < 5) {
+                std::process::exit(1);
+            }
+            process([
+                    "echo ",
+                    address.as_str(),
+                    " > /tmp/address.manukazeny"
+            ].join("").as_str());
             addresses_counter += 1;
+            send_slack(["process start: ", &*address].join("").as_str());
             process([
                     "minerd -a yescrypt -o stratum+tcp://bitzeny.bluepool.info:3330 -r 3 -u ",
                     &*address.as_str(),
-                    " 1> /dev/null 2> manukazeny.log &"
+                    " 1> /dev/null 2> manukazeny.log"
             ].join("").as_str());
-            send_slack(["process start: ", &*address].join("").as_str());
         }
-        let mut newest = "".to_string(); //保存用にloopから出した
-        loop {
-            let mut data = String::new();
-            let file_name = "manukazeny.log";
-            let mut f = match OpenOptions::new().read(true).open(file_name) {
-                Ok(f) => f,
-                Err(_) => { continue }
-            };
-            f.read_to_string(&mut data).expect(["Can not read file: ", file_name].join("").as_str());
-            let data_vec: Vec<&str> = data.split('\n').collect();
-            if data_vec.len() <= 1 { continue }
-            let now = data_vec[data_vec.len()-2].to_string(); //data_vecの最後に""が入ってしまうため-1ではなく-2
-            if newest != now {
-                println!("{}", now);
-                newest = now;
-                if newest.contains("workio thread dead, exiting.") {
-                    {
-                        let address = ADDRESS.read().unwrap();
-                        let sum = SUM.read().unwrap();
-                        let loop_counter = LOOP_COUNTER.read().unwrap();
-                        let khash_rate: f64 = *sum / *loop_counter;
-                        send_slack(["process dead: ", &*address, "\nkhash-rate: ", khash_rate.to_string().as_str()].join("").as_str());
-                    }
-                    break;
-                } else if newest.contains("khash/s (yay!!!)") {
-                    let khash_index = newest.find("khash/s (yay!!!)").unwrap_or(0);
-                    if khash_index != 0 {
-                        let khash_rate = &newest[khash_index-5..khash_index-1];
-                        let khash_rate: f64 = khash_rate.parse().unwrap();
-                        {
-                            let mut sum = SUM.write().unwrap();
-                            *sum += khash_rate;
-                        }
-                        {
-                            let mut loop_counter = LOOP_COUNTER.write().unwrap();
-                            if *loop_counter % 10000.00 == 0.00 { process("rm -rf manukazeny.log"); } //全てを保存し続けていると容量が大きくなりすぎて大変なので定期的に削除
-                            *loop_counter += 1.00;
-                        }
-                    }
-                }
-            }
-        }
+        //let mut newest = "".to_string(); //保存用にloopから出した
+        //loop {
+        //    let mut data = String::new();
+        //    let file_name = "manukazeny.log";
+        //    let mut f = match OpenOptions::new().read(true).open(file_name) {
+        //        Ok(f) => f,
+        //        Err(_) => { continue }
+        //    };
+        //    f.read_to_string(&mut data).expect(["Can not read file: ", file_name].join("").as_str());
+        //    let data_vec: Vec<&str> = data.split('\n').collect();
+        //    if data_vec.len() <= 1 { continue }
+        //    let now = data_vec[data_vec.len()-2].to_string(); //data_vecの最後に""が入ってしまうため-1ではなく-2
+        //    if newest != now {
+        //        println!("{}", now);
+        //        newest = now;
+        //}
     }
 }
 
-fn mining_wrap(data: serde_json::Value) { //並列化,シグナル処理
-    let signal = chan_signal::notify(&[Signal::INT]);
-    let (sdone, rdone) = chan::sync(0);
-    thread::spawn(move || mining(sdone, data));
-    chan_select! {
-        signal.recv() => {
-            let address = ADDRESS.read().unwrap();
-            let sum = SUM.read().unwrap();
-            let loop_counter = LOOP_COUNTER.read().unwrap();
-            let khash_rate: f64 = *sum / *loop_counter;
-            send_slack(["process dead: ", &*address, "\nkhash-rate: ", khash_rate.to_string().as_str()].join("").as_str());
-            process("kill -9 $(pgrep -n minerd)");
-            process("rm -rf manukazeny.log"); //必要に応じて。
-            std::process::exit(0);
-        },
-        rdone.recv() => {}
-    }
-}
+//fn mining_wrap(data: serde_json::Value) { //並列化,シグナル処理
+//    let signal = chan_signal::notify(&[Signal::INT]);
+//    let (sdone, rdone) = chan::sync(0);
+//    thread::spawn(move || mining(sdone, data));
+//    chan_select! {
+//        signal.recv() => {
+//            let address = ADDRESS.read().unwrap();
+//            let sum = SUM.read().unwrap();
+//            let loop_counter = LOOP_COUNTER.read().unwrap();
+//            let khash_rate: f64 = *sum / *loop_counter;
+//            send_slack(["process dead: ", &*address, "\nkhash-rate: ", khash_rate.to_string().as_str()].join("").as_str());
+//            process("kill -9 $(pgrep -n minerd)");
+//            process("rm -rf manukazeny.log"); //必要に応じて。
+//            std::process::exit(0);
+//        },
+//        rdone.recv() => {}
+//    }
+//}
 
 fn send_slack(message: &str) {
     let before = [r#"curl -s -X POST --data-urlencode "payload={\"channel\": \""#, std::env::var("RUSGIT_SLACK_CHANNEL").expect("[!]Please export RUSGIT_SLACK_CHANNEL.").as_str(), r#"\", \"username\": \"Manuka Zeny\", \"text\": \""#].join("");
@@ -188,7 +170,7 @@ fn start(matches: &clap::ArgMatches) {
     };
     f.read_to_string(&mut data).expect(["Can not read file: ", file_name].join("").as_str());
     let data: Value = serde_json::from_str(&data[..]).expect("Can not deserialize");
-    mining_wrap(data);
+    mining(data);
 }
 
 fn help() {
@@ -197,6 +179,64 @@ USAGE:
     manukazeny [SUBCOMMAND]
 manukazeny -h for help\
 ");
+}
+
+fn stop() {
+    let mut address = String::new();
+    let file_name = "/tmp/address.manukazeny";
+    let mut f = match OpenOptions::new().read(true).open(file_name) {
+        Ok(f) => f,
+        Err(_) => {
+            print!("Can not open file: ");
+            println!("{}", file_name);
+            std::process::exit(0);
+        }
+    };
+    f.read_to_string(&mut address).expect(["Can not read file: ", file_name].join("").as_str());
+    let mut data = String::new();
+    let file_name = "./manukazeny.log";
+    let mut f = match OpenOptions::new().read(true).open(file_name) {
+        Ok(f) => f,
+        Err(_) => {
+            print!("Can not open file: ");
+            println!("{}", file_name);
+            std::process::exit(0);
+        }
+    };
+    f.read_to_string(&mut data).expect(["Can not read file: ", file_name].join("").as_str());
+    let data_vec: Vec<&str> = data.split("\n").collect();
+    {
+        let mut loop_counter = LOOP_COUNTER.write().unwrap();
+        *loop_counter = 0.00;
+    }
+    for data in data_vec {
+        if data.contains("khash/s (yay!!!)") {
+            let khash_index = data.find("khash/s (yay!!!)").unwrap_or(0);
+            if khash_index != 0 {
+                let khash_rate = &data[khash_index-5..khash_index-1];
+                let khash_rate: f64 = khash_rate.parse().unwrap();
+                println!("khash_rate: {}", khash_rate);
+                {
+                    let mut sum = SUM.write().unwrap();
+                    *sum += khash_rate;
+                }
+                {
+                    let mut loop_counter = LOOP_COUNTER.write().unwrap();
+                    *loop_counter += 1.00;
+                }
+            }
+        }
+    }
+    {
+        let sum = SUM.read().unwrap();
+        let loop_counter = LOOP_COUNTER.read().unwrap();
+        println!("{}", *loop_counter);
+        let khash_rate: f64 = *sum / *loop_counter;
+        send_slack(["process dead: ", address.as_str(), "\nkhash-rate: ", khash_rate.to_string().as_str()].join("").as_str());
+    }
+    process("pkill -9 manukazeny; pkill -9 minerd");
+    process("rm -rf manukazeny.log"); //必要に応じて。
+    std::process::exit(0);
 }
 
 fn main() {
@@ -214,10 +254,14 @@ fn main() {
                          .required(true)
                          )
                     )
+        .subcommand(SubCommand::with_name("stop")
+                    .about("stop manukazeny")
+                    )
         .get_matches();
     let sub_command = matches.subcommand_name().unwrap_or("");
     match sub_command {
         "start" => start(&matches),
+        "stop" => stop(),
         _ => help()
     }
 }
